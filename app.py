e7092632df01d65d810224c9706959840094e98d
nantrim <nantrim@udel.edu>
2023-11-08 13:10:42 -0500

    trained up to question 55

--- app.py
+++ app.py
@@ -1,20 +1,19 @@
 from flask import Flask, render_template, request, jsonify
-
 from chat import get_response
 
 app = Flask(__name__)
 
-@app.get("/")
+@app.route("/", methods=["GET"])
 def index_get():
     return render_template("base.html")
 
-@app.post("/predict")
-def.predict():
+@app.route("/predict", methods=["POST"])
+def predict():
     text = request.get_json().get("message")
-    #TODO: check if text is valid 
+    # TODO: check if text is valid
     response = get_response(text)
-    message = {"answer":response}
-    return jsonify (message)
+    message = {"answer": response}
+    return jsonify(message)
 
-if__name__ == "__main__"
+if __name__ == "__main__":
     app.run(debug=True)
--- chat.py
+++ chat.py
@@ -8,12 +8,12 @@ from nltk_utils import bag_of_words, tokenize
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-with open('intents.json', 'r') as json_data:
+with open('intents.json', 'r', encoding='utf-8') as json_data:
     intents = json.load(json_data)
 
 FILE = "data.pth"
 data = torch.load(FILE)
-
+    
 input_size = data["input_size"]
 hidden_size = data["hidden_size"]
 output_size = data["output_size"]
@@ -25,7 +25,7 @@ model = NeuralNet(input_size, hidden_size, output_size).to(device)
 model.load_state_dict(model_state)
 model.eval()
 
-bot_name = "Sam"
+bot_name = "Edward"
 
 def get_response(msg):
     sentence = tokenize(msg)
